295d1c9ca576416b1968f37378a10f54a95722a3
Dnmrk4 <danmark.chemuren@gmail.com>
2019-06-17 11:17:20 +0300

    Add of documentations in the  password-locker file.

--- password_locker.py
+++ password_locker.py
@@ -8,30 +8,46 @@ class Password:
     password_list = []  # list to store user  passwords
 
     def _init_(self, account, username, password):
-        
+        """
+        This function will allow the user to create instances of the class with unique details in each instance
+        """
         self.account = account
         self.username = username
         self.password = password
 
     def save_password(self):
-        
+        """
+        This function will add users password to the password array
+        """
         password.password_locker.append(self)
 
     @classmethod
     def display_passwords(cls):
-        
+        """
+        This function returns all passwords on the list
+        """
         return cls.password_list
 
     @classmethod
     def delete_password(cls, account):
-       
+        """
+        This function will delete the users password in the list
+        Args:
+             This is the account of the password the user wants to delete
+        """
         for password in cls.password:
             if password.account.lower() == account.lower():
                 cls.password_list.remove(password)
 
     @classmethod
     def password_exist(cls, account):
-        
+        """
+        This function checks weather the password of the account exist
+        Args:
+             acc- this is the acount which user wants to conferm if its password exist
+        return:
+               True or False
+        """
         for password in cls.password_list:
             if password.account.lower() == account.lower():
                 return True
