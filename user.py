72c587fc4be9964c9b3776bf559cb2a6c5ef4c96
Dnmrk4 <danmark.chemuren@gmail.com>
2019-06-17 11:45:43 +0300

    Update of some documentations in the  user .py file.

--- user.py
+++ user.py
@@ -1,11 +1,21 @@
 class User:
-   
+    """
+    This class will contain all details of the user
+    """
     def _init_(self,login,password):
-      
+      """
+      This will create unique details for each of the user class
+      """
       self.login = login
       self.password = password
 
     def user_exist(self,password):
-      
+      """
+      This will use the password to authenticate the user before showing the passwords
+      Args:
+           the user password
+      return:
+             boolean
+      """
       if self.password == password:
     return True
